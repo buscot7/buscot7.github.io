3e7e09a2d234cb251228c8ad6f4b5192604d0d8f
buscot7 <buscot7@gmail.com>
2017-10-13 12:40:17 +0200

    findprecious

---
+++ template.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" 
+          content="Doge Club a vu jour afin de rassembler l'ensemble des entreprises 
+                présentes au sein du Doge afin d'assurer une harmonie entre les différents acteurs. Et de donnée vie aux deux bâtiments 
+                en faisant en sorte que tout le monde puissent s'investire pour le bien-être de tous."
+    >
+    <meta name="author" content="Wild Code School Lille">
+
+    <title>Doge Club</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <?php
+        include('header.php');
+    ?>
+
+    <!-- Banner -->
+    <header class="masthead">
+        <span id="top-top"></span>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-heading">Le Doge Club <br><br>Regroupement d'entreprises</div>
+                <div>
+                  <a href="#main">
+                    <span class="fa-stack fa-5x">
+                      <i class="fa fa-chevron-down fa-stack-1x fa-inverse"></i>
+                    </span>
+                  </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Initiative -->
+    <section class="bg-light section-index" id="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="title-index title-blue" id="title-blue">Une initiative pour</h2>
+                    <h3 class="section-subheading">
+                      Le projet Doge Club a vu jour afin de rassembler l'ensemble des entreprises 
+                      présentes au sein du Doge afin d'assurer une harmonie entre les différents acteurs. Et de donnée vie aux deux bâtiments 
+                      en faisant en sorte que tout le monde puissent s'investire pour le bien-être de tous.
+                    </h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-users fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Rassembler</h4>
+                    <p class="text-muted">Afin de créer des relations durables entres chaques acteurs au seins du batiment.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-calendar-check-o fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Evenements</h4>
+                    <p class="text-muted">Organiser des évenements pour garder une dynamique et donner vie a la structure.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-heart fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Vie</h4>
+                    <p class="text-muted">Donner vie a la structure.</p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-bullhorn fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Communiquer</h4>
+                    <p class="text-muted">Organisation de meet-up accessible aux personnes externe au club.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-id-card fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Listing</h4>
+                    <p class="text-muted">Présentation des entreprises présentes sur l'enssemble du batiments, ainsi que leurs activitées.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fa fa-circle fa-stack-2x text-success"></i>
+                      <i class="fa fa-circle-o-notch fa-spin fa-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Evoluer</h4>
+                    <p class="text-muted">Présenter chacunes des entreprises, leurs activitées.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- environnment -->
+    <section class="section-index">
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="title-index">L'environnment du Doge Club</h2>
+            </div>
+        </div>
+
+        <!-- presentation doge club -->
+        <div class="row align-items-center">
+          <div class="col-md-5 order-2">
+            <div class="p-5">
+              <img class="img-fluid rounded-circle " src="img/environment/doge.jpg" alt="batiment doge">
+            </div>
+          </div>
+          <div class="col-md-5 order-1">
+            <div class="section-white">
+              <h4 class="title-env">Le Doge implanté en plein coeur du pôle d’excellence régional Euratechnologies à Lille.</h4>
+              <p>
+                <i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true"></i>
+                Une superficie de 5 000 m² et s’articule autour de deux bâtiments proposant des plateaux fonctionnels 
+                de standing répondant aux exigences des entreprises des nouvelles technologies de l’information et de la communication.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- presentation euratechnologie -->
+    <section class="bg-light">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5">
+            <div class="p-5">
+              <img class="img-fluid rounded-circle" src="img/environment/euratechnologie.jpg" alt="photo-batiment-euratechnologie">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-5">
+              <h4 class="title-env">Euratechnologie c'est un écosystème digital avec 300 entreprises.</h4>
+              <p>
+                <i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true"></i>
+                EuraTechnologies accompagne le développement de tous les entrepreneurs du numérique grâce à une méthodologie et un savoir-faire 
+                unique dans le soutien aux entreprises, de leur amorçage à leur déploiement international. Créé en 2009, EuraTechnologies a été classé 
+                dans le top 10 des accélérateurs d’Europe par Fundacity, et le 1er en France !
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- presentation kayak -->
+    <section>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-5 order-2">
+            <div class="p-5">
+              <img class="img-fluid rounded-circle" src="img/environment/kayak.jpg" alt="logo-kayak communication">
+            </div>
+          </div>
+          <div class="col-md-6 order-1">
+            <div class="section-white">
+              <h4 class="title-env">Le Doge club by Kayak.</h4>
+              <p>
+                <i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true"></i>
+                25 ans d’expertise, d’attention et de créativité, de la stratégie à l’opérationnel et de la communication interne à l’externe, 
+                répondans aux attentes des marques régionales, nationales ou internationales à travers des prestations online et offline..
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- chevron -->
+      <div class="container">
+      <div id="chevron-up">
+          <a href="#top-top">
+            <span class="fa-stack fa-3x">
+              <i class="fa fa-chevron-up fa-stack-1x fa-inverse"></i>
+            </span>
+          </a>
+      </div>
+    </div>
+    </section>
+    
+
+    <?php
+        include('footer.php');
+    ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts -->
+    <script src="js/agency.min.js"></script>
+
+</body>
+
+</html>
